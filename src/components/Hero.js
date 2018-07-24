bf2acd0c8bc48c0ed42596f523373b5cfb5811c0
Steve Smith <stevesmithsays@gmail.com>
2018-07-24 12:26:42 -0500

    updated git ignore

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,7 +6,7 @@ const Hero = () => {
     <div className="hero-component">
       <div className="hero-body">
         <div className="hero-container">
-          <h1 className="hero-hwun hedr">Spilled Milk</h1>
+          <h1 className="hero-hwun hedr">Spilled Milk?</h1>
           <h2 className="hero-htwo hedr">No Worries.</h2>
         </div>
       </div>
