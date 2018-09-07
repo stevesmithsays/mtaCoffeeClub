1132bbbef4903ef0642ab8f971efd3c03274487b
Steve Smith <stevesmithsays@gmail.com>
2018-09-06 22:16:01 -0500

    updated m mobile hamburger menu animation

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,36 @@ import React from 'react';
 import '../styles/scss/Header.scss';
 import { Link } from 'react-router-dom';
 
+let msg = 'menu button'
+
+const alertFunc = () => {
+ console.log(msg);
+  alert(msg);
+}
+
+const menuFunc = () => {
+  document.getElementById("dropdown").classList.toggle("show");
+  console.log(menuFunc, "menuFunc");
+}
+
+window.onClick = function(event) {
+  if(!event.target.matches(".menu-btn")){
+    let dropDowns = document.getElementByClassName('dropdown-content');
+    for(let i =0; i<dropDowns.length; i++){
+      let openDrop = dropDowns[i];
+      if(openDrop.classList.contains('show')){
+        openDrop.classList.remove('show');
+      }
+    }
+  }
+}
+
 
 const Header = () => {
   return(
     <div className="header-component">
       <div className="header-body">
-      <Link to="/home">
+      <Link to="/home" className="logo-container">
           <div className="site-logo">
             <div className="logo-bod">
               <span className="logo-span">Mount Atlas</span>
@@ -15,8 +39,8 @@ const Header = () => {
           </div>
         </Link>
         <div className="nav-container">
-          <nav className="site-nav">
-            <ul>
+          <nav className="site-nav" id="dropdown">
+            <ul className="dropdown-content">
               <li><Link to="/about">About</Link></li>
               <li><Link to="/blog">Blog</Link></li>
               <li><Link to="/schedule">Schedule</Link></li>
@@ -25,7 +49,7 @@ const Header = () => {
           </nav>
           </div>
         <div className="menu-btn-container">
-          <button className="menu-btn">V</button>
+          <div className="menu-btn" onClick={menuFunc}></div>
         </div>
       </div>
     </div>
